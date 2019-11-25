bd3e7e7816c89ea5b625bed5102ab3453094688a
wtkn25 <test@example.com>
2019-11-25 20:41:33 +0900

    マウント先とダウンロード先を一致させたらダウンローできた

--- script/sample3.py
+++ script/sample3.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 import datetime
 
+
 def execSearch(browser: webdriver):
     """
     Googleで検索を実行する
@@ -18,22 +19,33 @@ def execSearch(browser: webdriver):
     browser.get('http://jusyo.jp/csv/new.php')
     sleep(5)
 
-    browser.find_element_by_xpath('//*[@id="dl-table"]/table[1]/tbody/tr[2]/td/span[2]/a').click()
+    browser.find_element_by_xpath(
+        '//*[@id="dl-table"]/table[1]/tbody/tr[2]/td/span[2]/a').click()
 
     sleep(5)
 
 
-
-
 if __name__ == '__main__':
     try:
-        #browser = webdriver.Firefox()  # 普通のFilefoxを制御する場合
-        #browser = webdriver.Chrome()   # 普通のChromeを制御する場合
+        # browser = webdriver.Firefox()  # 普通のFilefoxを制御する場合
+        # browser = webdriver.Chrome()   # 普通のChromeを制御する場合
+        options = webdriver.ChromeOptions()
+        prefs = {}
+        prefs['download.default_directory'] = '/home/seluser/Downloads/test'
+        prefs['download.directory_upgrade'] = True
+        prefs['download.extensions_to_open'] = ''
+        prefs['download.prompt_for_download'] = False
+        prefs['safebrowsing.enabled'] = True
+        options.add_experimental_option("prefs", prefs)
+        desired_capabilities = options.to_capabilities()
 
         # HEADLESSブラウザに接続
         browser = webdriver.Remote(
             command_executor='http://selenium-hub:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.CHROME)
+            desired_capabilities=desired_capabilities)
+        # desired_capabilities=DesiredCapabilities.CHROME)
+
+        # HEADLESSブラウザに接続
 
         # Googleで検索実行
         execSearch(browser)
@@ -42,4 +54,3 @@ if __name__ == '__main__':
         # 終了
         browser.close()
         browser.quit()
-
